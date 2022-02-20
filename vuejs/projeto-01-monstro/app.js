16246250566cfb948f640f08b5b7d648288d0fb5
Marcos Sousa <kimura.marcos@gmail.com>
2022-02-20 13:06:55 -0300

    customizing result and battle score panels

--- vuejs/projeto-01-monstro/app.js
+++ vuejs/projeto-01-monstro/app.js
@@ -1,4 +1,14 @@
 new Vue({
-    el: '#app',
-   
-})
+  el: "#app",
+  data: {
+    playerLife: 100,
+    monsterLife: 100,
+  },
+  computed: {
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0;
+    },
+  },
+  methods: {},
+  watch: {},
+});
