d8a7ade33401048a4d3bb742bc9478e1727d0f84
Mark Sousa <kimura.marcos@gmail.com>
2022-02-23 17:57:09 -0300

    Atualizando Repositorio

--- vuejs/projeto-01-monstro/app.js
+++ vuejs/projeto-01-monstro/app.js
@@ -1,14 +1,21 @@
 new Vue({
   el: "#app",
   data: {
+    running: 0,
     playerLife: 100,
     monsterLife: 100,
   },
   computed: {
-    hasResult() {
+    hasResult: {
       return this.playerLife == 0 || this.monsterLife == 0;
-    },
+    }
   },
-  methods: {},
+  methods: {
+    startGame() {
+      this.running = true,
+        this.monsterLife = 100,
+        this.playerLife = 100
+    }
+  },
   watch: {},
 });
