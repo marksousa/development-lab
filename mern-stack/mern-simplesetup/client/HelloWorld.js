c704465e4bc94d9d3716f59f96fe4b2d83dd35ba
Marcos Sousa <kimura.marcos@gmail.com>
2022-02-27 23:17:38 -0300

    Learning MERN

---
+++ mern-stack/mern-simplesetup/client/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Wake Up Neo! Follow the white rabbit!</h1>
+    </div>
+  );
+};
+
+export default hot(module)(HelloWorld);
