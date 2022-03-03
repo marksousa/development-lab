2b1bf35712af7681d02c9d5c0d587c73218790c0
Marcos Sousa <kimura.marcos@gmail.com>
2022-03-02 23:04:02 -0300

    Coffee Machine first methods

--- python/udemy-drangelayu-bootcamp-study/coffe_machine/main.py
+++ python/udemy-drangelayu-bootcamp-study/coffe_machine/main.py
@@ -1,32 +1,87 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
+from data import MENU
+from data import resources
+import os
+
+clear = lambda: os.system('clear')
+
+clear()
+
+machine_on = True
+
+coins_accepted = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01
 }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+till_amount = {
+    "money": 0
 }
 
+def client_input(msg = "What would you like? (espresso/latte/cappuccino):"):
+    """ Display an input, validate the players choice and returns the corresponding choice. """
+    client_choice = input(msg).lower()
+
+    if client_choice in MENU.keys(): return selling_flow(MENU.get(client_choice))
+    if client_choice == 'report': return generate_resources_report()
+    if client_choice == 'off': return turn_off()
+    print("Invalid Option. Try again.")
+    return client_input()
+
+def turn_off():
+    machine_on = False
+    quit()
+
+def generate_resources_report():
+    for eachResource in resources.keys():
+        print(f"{ eachResource.capitalize() }: {resources.get(eachResource)}")
+    get_till_amount()
+
+def get_till_amount():
+    return print(f"Money: {till_amount.get('money')}")
+
+def verify_disponibility(drink):
+    resource = MENU.get(drink)
+    return check_resources(resource["ingredients"])
+
+def check_resources(ingredients):
+    for resource in resources:
+        if resources[resource] < ingredients[resource] : return False
+    return True
+
+def process_coins():
+    print("Please insert coins.")
+    quarters = int(input("how many quarters?: "))
+    dimes = int(input("how many dimes?: "))
+    nickles = int(input("how many nickles?: "))
+    pennies = int(input("how many pennies?: "))
+
+    client_amount_coin = quarters * coins_accepted["quarters"] + dimes * coins_accepted["dimes"] + nickles * coins_accepted["nickles"] + pennies * coins_accepted["pennies"]
+    return client_amount_coin
+
+def selling_flow(client_choice):
+    # 1. Check the resources to confirm if drink is available
+    # 2. Customer insert the coins
+    # 3. Check the transaction($)
+        # 3.1. If >=, calculate the change, deduct the ingredients from the resources inventory and prepare the drink,
+        # 3.2. If No, "Print "Sorry that's not enough money. Money refunded."
+    if client_choice :
+        client_wallet = process_coins()
+        if client_wallet >= int(client_choice["cost"]):
+            print("Ok.")
+            print(client_choice)
+        else :
+            print("Not enough.")
+
+def deduct_resources(drink):
+    resources = MENU.get(drink)
+    ingredients = resources["ingredients"]
+
+# def prepare_drink(drink):
+#     print(MENU.get(drink))
+
+
+while machine_on:
+    drink_available = client_input()
+    selling_flow(drink_available)
