36620c6c15b67963755294eaab5a58f6cf1066d7
Mark Sousa <kimura.marcos@gmail.com>
2022-03-05 00:44:56 -0300

    cls()/clear() def function cross-platform

--- python/README.md
+++ python/README.md
@@ -1,6 +1,6 @@
 #Python Notes
 
-################### Scope ####################
+## Scope
 
 enemies = 1
 
@@ -11,55 +11,51 @@ print(f"enemies inside function: {enemies}")
 increase_enemies()
 print(f"enemies outside function: {enemies}")
 
-# #Local Scope
+#Local Scope
 
-# def drink_potion():
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
 
-# potion_strength = 2
-
-# print(potion_strength)
-
-# drink_potion()
+drink_potion()
 
 #Global scope
 player_health = 10
 
 def game():
-def drink_potion():
-potion_strength = 2
-print(potion_strength)
-drink_potion()
+    def drink_potion():
+        potion_strength = 2
+        print(potion_strength)
+    drink_potion()
 
 print(player_health)
 
-# #There is no Block Scope
+# OBS: There is no Block Scope
 
 Remember that in Python there is no block scope. Inside a if/else/for/while code block is the same as outside it.
-
-# if 3 > 2:
-
-# a_variable = 10 # here we have global scope.. inside a if
+Exemplo: 
 
 game_level = 3
 def create_enemie():
-enemies = ['Skeleton', 'Zombie', 'Alien']
-if game_level < 5 :
-new_enemy = enemies[0] # "new_enemy" is inside a if but has scope inside create_enemie function. No Block Scope.
+    enemies = ['Skeleton', 'Zombie', 'Alien']
+    if game_level < 5 :
+        new_enemy = enemies[0] # "new_enemy" is inside a if but has scope inside create_enemie function. There is No Block Scope.
 
 print(new_enemy)
 
 #Modifying Global Scopes
+exemplo:
+
 enemies = 1
 
 def increase_enemies():
-print(f"Enemies inside the function {enemies}")
-return enemies + 1
+    print(f"Enemies inside the function {enemies}")
+    return enemies + 1
 
 enemies = increase_enemies()
 print(f"Enemies outside the function {enemies}")
 
 #Global Constants
-
 PI = 3.14159
 URL = "https://www.google.com"
 TWITTER_HANDLE = "@iammarksousa"
--- python/udemy-drangelayu-bootcamp-study/coffe_machine/main.py
+++ python/udemy-drangelayu-bootcamp-study/coffe_machine/main.py
@@ -2,9 +2,8 @@ from data import MENU
 from data import resources
 import os
 
-clear = lambda: os.system('clear')
-
-clear()
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 machine_on = True
 
@@ -23,7 +22,7 @@ def client_input(msg = "What would you like? (espresso/latte/cappuccino):"):
     """ Display an input, validate the players choice and returns the corresponding choice. """
     client_choice = input(msg).lower()
 
-    if client_choice in MENU.keys(): return selling_flow(MENU.get(client_choice))
+    if client_choice in MENU.keys(): return selling_flow({client_choice: MENU.get(client_choice)})
     if client_choice == 'report': return generate_resources_report()
     if client_choice == 'off': return turn_off()
     print("Invalid Option. Try again.")
@@ -34,9 +33,12 @@ def turn_off():
     quit()
 
 def generate_resources_report():
+    get_resources_inventory()
+    get_till_amount()
+
+def get_resources_inventory():
     for eachResource in resources.keys():
         print(f"{ eachResource.capitalize() }: {resources.get(eachResource)}")
-    get_till_amount()
 
 def get_till_amount():
     return print(f"Money: {till_amount.get('money')}")
@@ -48,7 +50,7 @@ def verify_disponibility(drink):
 def check_resources(ingredients):
     for resource in resources:
         if resources[resource] < ingredients[resource] : return False
-    return True
+    return True 
 
 def process_coins():
     print("Please insert coins.")
@@ -60,19 +62,20 @@ def process_coins():
     client_amount_coin = quarters * coins_accepted["quarters"] + dimes * coins_accepted["dimes"] + nickles * coins_accepted["nickles"] + pennies * coins_accepted["pennies"]
     return client_amount_coin
 
-def selling_flow(client_choice):
+def selling_flow(valid_client_choice):
+    print(valid_client_choice.keys())
+    
     # 1. Check the resources to confirm if drink is available
-    # 2. Customer insert the coins
-    # 3. Check the transaction($)
-        # 3.1. If >=, calculate the change, deduct the ingredients from the resources inventory and prepare the drink,
-        # 3.2. If No, "Print "Sorry that's not enough money. Money refunded."
-    if client_choice :
-        client_wallet = process_coins()
-        if client_wallet >= int(client_choice["cost"]):
-            print("Ok.")
-            print(client_choice)
-        else :
-            print("Not enough.")
+    # # 2. Customer insert the coins
+    # # 3. Check the transaction($)
+    #     # 3.1. If >=, calculate the change, deduct the ingredients from the resources inventory and prepare the drink,
+    #     # 3.2. If No, "Print "Sorry that's not enough money. Money refunded."
+    # if valid_client_choice :
+    #     client_wallet = process_coins()
+    #     if client_wallet >= int(valid_client_choice["cost"]):
+    #         print("Ok.")
+    #     else :
+    #         print("Not enough.")
 
 def deduct_resources(drink):
     resources = MENU.get(drink)
@@ -81,7 +84,5 @@ def deduct_resources(drink):
 # def prepare_drink(drink):
 #     print(MENU.get(drink))
 
-
 while machine_on:
-    drink_available = client_input()
-    selling_flow(drink_available)
+    client_input()
