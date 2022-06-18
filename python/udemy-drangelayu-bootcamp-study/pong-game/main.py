31edf5f435eba2e840a6b5ab42a082097f3a0508
marksousa <sousa.marcosrk@gmail.com>
2022-06-18 15:29:21 -0300

    Adição de arquivos par ao pong game em python

--- python/udemy-drangelayu-bootcamp-study/pong-game/main.py
+++ python/udemy-drangelayu-bootcamp-study/pong-game/main.py
@@ -1,10 +1,32 @@
-from turtle import Turtle, Screen
+import time
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+
+game_is_on = True
+MOVE_DISTANCE = 20
 
 screen = Screen()
 screen.title("Pong Game")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
 
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(right_paddle.move_up, "Up")
+screen.onkey(right_paddle.move_down, "Down")
+screen.onkey(left_paddle.move_up, "w")
+screen.onkey(left_paddle.move_down, "s")
+
+
+while game_is_on:
+    screen.update()
+    ball.move()
+    time.sleep(0.1)
 
 
 screen.exitonclick()
